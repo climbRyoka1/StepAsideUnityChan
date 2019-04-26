736f7d403a67f5d28acb165549f0462ca64e546f
climbRyokawa1 <s.tempter.e2411@gmail.com>
2019-04-27 06:19:49 +0900

    StepAsideUnityChan コミット
    時間経過でアイテムを生成するようにしました

---
+++ Assets/Lesson6/ItemGenerator.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemGenerator : MonoBehaviour {
+	
+	public GameObject carPrefab;
+	public GameObject coinPrefab;
+	public GameObject conePrefab;
+
+	private GameObject unityChan;
+
+	private float posRange = 3.4f;
+	private float run;
+	private float elapsedTime;
+
+
+	// Use this for initialization
+	void Start () {
+		this.unityChan = GameObject.Find ("unitychan");
+	}
+
+	void Update () {
+		this.run = this.unityChan.transform.position.z;
+		if (-210.0f < run && run < 70.0f) {
+			this.elapsedTime += Time.deltaTime;
+			if (elapsedTime > 1.0f) {
+				elapsedTime = 0;
+				appear ();
+			}
+		}
+	}
+
+	void appear(){
+			int num = Random.Range (1, 11);
+			if (num <= 2) {
+				for (float j = -1; j <= 1; j += 0.4f) {
+				GameObject cone = Instantiate (conePrefab) as GameObject;
+				cone.transform.position = new Vector3 (4 * j, cone.transform.position.y,run + 50 );
+				}
+			} else {
+				for (int j = -1; j <= 1; j++) { 
+					int item = Random.Range (1, 11);
+					int offsetZ = Random.Range (-5, 6);
+				    if (1 <= item && item <= 6){
+						GameObject coin = Instantiate (coinPrefab) as GameObject;
+						coin.transform.position = new Vector3 (posRange * j, coin.transform.position.y, run + 50 + offsetZ);
+					} else if (7 <= item && item <= 9) {
+						GameObject car = Instantiate (carPrefab) as GameObject;
+						car.transform.position = new Vector3 (posRange * j, car.transform.position.y, run + 50 + offsetZ);
+					}
+				}
+			}
+		}
+	}	
